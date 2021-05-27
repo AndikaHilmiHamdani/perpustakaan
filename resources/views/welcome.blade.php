9be1b2004f2c1731508c2ecefd4e9268d5148df7
AndikaHilmiHamdani <andikahilmi7201@gmail.com>
2021-05-27 14:03:41 +0700

    flexing content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,69 +2,84 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
-    <title>Perpustakaan Online</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" type="text/css" href="/css/style.css">
+  <title>Perpustakaan Online</title>
 </head>
 
 <body>
 
-    <div id="container">
-        <div class="header"><img src="" width="100" height="100">
-            <h1>Perpustakaan JTI Polinema</h1>
-        </div>
-
-        <div class="main">
-            <aside>
-            <div class="left">
-                <h3 class="text-center">MENU</h3>
-                <ul>
-                    <li><a href="http://localhost/perpus/login.php">Login</a></li>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Daftar Buku</a></li>
-                </ul>
-            </div>
-            </aside>
-            
-            <article>
-            <div class="middle">
-                <h3 class="text-center">Artikel/Berita/Buku Terbaru</h3>
-                <h2 class="text-center">Buku: Rahvayana - Aku Lala Padamu</h2><br>
-                <div class="text-center">
-                    <img src="/img/logo.jpg" width="240">
-                </div>
-                <p><a href="#">Baca Selengkapnya >></a></p>
-            </div>
-            <div class="middle">
-                <h3 class="text-center">Artikel/Berita/Buku Terbaru</h3>
-                <h2 class="text-center">Buku: Rahvayana - Aku Lala Padamu</h2><br>
-                <div class="text-center">
-                    <img src="/img/logo.jpg" width="240">
-                </div>
-                <p><a href="#">Baca Selengkapnya >></a></p>
-            </div>
-            </article>
-            
-            <aside>
-            <div class="right">
-                <h3 class="text-center">BUKU TERPOPULER</h3>
-                <ul>
-                    <li><a href="#">Web Design</a></li>
-                    <li><a href="#">Pemrograman</a></li>
-                    <li><a href="#">Database</a></li>
-                </ul>
-            </div>
-            </aside>
-            
-        </div>
+  <div id="container">
+    <div class="header"><img src="" width="100" height="100">
+      <h1>Perpustakaan JTI Polinema</h1>
     </div>
 
-    <div class="footer">
-        <p class="text-center">Copyright © 2021 - JTI Polinema</a></p>
+    <div class="main">
+      <aside>
+        <div class="left">
+          <h3 class="text-center">MENU</h3>
+          <ul>
+            <li><a href="http://localhost/perpus/login.php">Login</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Daftar Buku</a></li>
+          </ul>
+        </div>
+      </aside>
+
+      <article>
+        <div class="middle d-flex flex-column p-2">
+          <div class="mx-auto" >
+            <h3 class="text-center">Artikel/Berita/Buku Terbaru</h3>
+            <h2 class="text-center">Buku: Rahvayana - Aku Lala Padamu</h2><br>
+            <div class="text-center">
+              <img src="/img/logo.jpg" width="240">
+            </div>
+            <p><a href="#">Baca Selengkapnya >></a></p>
+          </div>
+          <div class="mx-auto container-fluid" >
+            <h3 class="text-center">Artikel/Berita/Buku Terbaru</h3>
+            <h2 class="text-center">Buku: Rahvayana - Aku Lala Padamu</h2><br>
+            <div class="text-center">
+              <img src="/img/logo.jpg" width="240">
+            </div>
+            <p><a href="#">Baca Selengkapnya >></a></p>
+          </div>
+          <div class="mx-auto container-fluid" >
+            <h3 class="text-center">Artikel/Berita/Buku Terbaru</h3>
+            <h2 class="text-center">Buku: Rahvayana - Aku Lala Padamu</h2><br>
+            <div class="text-center">
+              <img src="/img/logo.jpg" width="240">
+            </div>
+            <p><a href="#">Baca Selengkapnya >></a></p>
+          </div>
+        </div>
+        
+      </article>
+
+      <aside>
+        <div class="right">
+          <h3 class="text-center">BUKU TERPOPULER</h3>
+          <ul>
+            <li><a href="#">Web Design</a></li>
+            <li><a href="#">Pemrograman</a></li>
+            <li><a href="#">Database</a></li>
+          </ul>
+        </div>
+      </aside>
+
     </div>
-    </div>
+  </div>
+
+  <div class="footer">
+    <p class="text-center">Copyright © 2021 - JTI Polinema</a></p>
+  </div>
+  </div>
 </body>
 
 </html>
