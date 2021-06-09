6fb049be73db4e092e3ef8ce64a458f2fb5086be
Shafriza <sahurgaming@gmail.com>
2021-06-09 11:12:49 +0700

    create db status and connection

---
+++ database/migrations/2021_06_09_031444_add_columntransaksi_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumntransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transaksi', function (Blueprint $table) {
+            $table->unsignedBigInteger('status_id')->nullable();
+            $table->index('status_id');
+            $table->foreign('status_id')->references('id')->on('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
